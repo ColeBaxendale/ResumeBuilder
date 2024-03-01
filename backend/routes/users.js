94cea3bba554c7f326c08bd5ad969be32fe0678a
Cole Baxendale <cbaxendale99@gmail.comm>
2024-03-01 01:45:51 -0500

    input validation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,8 +8,14 @@ const { body, validationResult } = require('express-validator');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Enter a valid email address'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+    body('password')
+      .isLength({ min: 8, max: 100 })
+      .withMessage('Password must be at least 8 characters long and no more than 100 characters')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+      .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character')
   ], userController.addUser);
+
+  
 router.post('/login', userController.loginUser); // Login route for users
 router.get('/profile/:userId', userController.getUser); // Get a user's profile
 router.put('/profile/:userId', userController.updateUser); // Update user profile
