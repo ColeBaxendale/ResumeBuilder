f50e63a974945129d6d550763f6f31e4fa55d1c9
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2024-03-02 23:19:04 -0500

    angular init, home page, navbar

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DonateComponent } from './pages/donate/donate.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent, pathMatch: 'full' },
+    { path: 'login', component: LoginComponent, pathMatch: 'full' }, // Removed the extra space here
+    { path: 'register', component: RegisterComponent, pathMatch: 'full' }, // Removed the extra space here
+    { path: 'about', component: AboutComponent, pathMatch: 'full' }, // Removed the extra space here
+    { path: 'donate', component: DonateComponent, pathMatch: 'full' }, // Removed the extra space here
+    // { path: 'stock/:symbol', component: StockPageComponent, canActivate: [AuthGuard] },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    // { path: '**', component: PageNotFoundComponent },
+];
